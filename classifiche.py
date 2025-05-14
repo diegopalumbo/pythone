b213d8d2efdb905d205bce6ab2169649808d86f1
diegopalumbo <94850262+diegopalumbo@users.noreply.github.com>
2025-05-14 08:59:54 +0200

    classifiche con output formattato in forma tabellare

--- classifiche.py
+++ classifiche.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from tabulate import tabulate
 
 options = Options()
 # options.add_argument('--headless')
@@ -15,14 +16,52 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 result = driver.get("https://live-tennis.eu/it/classifica-atp-live")
 try:
+    
     # Wait for the table to be present
     element = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.ID, 'u868'))
     )
+    
+    table_data = []
+    
+    # recupero elemento con id u868
+    el_table = driver.find_element(By.ID, 'u868')
+    
+    # da el_table recupero tbhead 
+    el_theads = el_table.find_elements(By.CLASS_NAME, 'tbhead')
+    
+    # prendo ultimo elemento di tbhead come header e lo aggiungo alla lista table_data
+    el_thead = el_theads[-1]
+    
+    # scorro le righe di thead e le aggiungo alla lista table_data
+    cells = el_thead.find_elements(By.TAG_NAME, 'td')
+    table_data.append([cell.text for cell in cells])
+
+    # da el_table recupero tbbody
+    el_tbody = el_table.find_elements(By.TAG_NAME, 'tbody')        
+    for tbody in el_tbody:  # Iterate over each tbody element
+        for row in tbody.find_elements(By.TAG_NAME, 'tr')[:100]:  # Find rows within each tbody
+            cells = row.find_elements(By.TAG_NAME, 'td') 
+            if cells[0].text.startswith("<") or cells[0].text.startswith(">") or cells[0].text.startswith("Tutti") or cells[0].text == "" :
+                continue            
+            table_data.append([cell.text for cell in cells])
+
+    # Cerca intestazione tramite classe tbhead 
+    # table_data = []
+    # for row in element.find_elements(By.CLASS_NAME, 'tbhead')[:100]:
+    #     cells = row.find_elements(By.TAG_NAME, 'td') 
+    #     table_data.append([cell.text for cell in cells])
+    
+    
     # Print the table element
-    for row in element.find_elements(By.TAG_NAME, 'tr'):
-        print(row.text)
+    # table_data = []
+    # for row in element.find_elements(By.TAG_NAME, 'tr')[:100]:
+    #     cells = row.find_elements(By.TAG_NAME, 'td') 
+    #     table_data.append([cell.text for cell in cells])
+    
+    print(tabulate(table_data, headers="firstrow", tablefmt="grid"))
+        
 except Exception as e:
-    print("Error:", e)
+    print(f"An error of type {type(e).__name__} occurred. Details: {e}")
 finally:
     driver.close()
